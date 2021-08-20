f3bc049dae8de66ec1976787bc0015c32d27f3ad
Grant Tannert <grtmega10@gmail.com>
2021-08-20 11:55:01 +0700

    Update ReverseInteger.java

--- Easy/ReverseInteger.java
+++ Easy/ReverseInteger.java
@@ -1,5 +1,8 @@
-// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside 
 
+// the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// using modulo to pop out the last integer and then * 10 + that value, to get the reverse.
 class Solution {
     public int reverse(int x) {
         long ans = 0;
