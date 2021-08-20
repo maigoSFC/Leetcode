014e76aedbc50e24ec7e9abf786e32f6d46f5303
Grant Tannert <grtmega10@gmail.com>
2021-08-20 11:56:50 +0700

    Update PalindromeNumber.java

--- Easy/PalindromeNumber.java
+++ Easy/PalindromeNumber.java
@@ -2,6 +2,9 @@
 
 // An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
+// get the second half of the integer in reverse, by the reverse integer method ( % & * 10 + pop) then compare the leftover integer with
+// the one you reversed. have to make /10 case as well.
+
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) {
