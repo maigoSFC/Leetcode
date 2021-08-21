640a6530edbb9dfd3aa1b8a8b88c2af323bc3d7a
Grant Tannert <grtmega10@gmail.com>
2021-08-21 16:59:23 +0700

    Update TwoSum.java

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -4,21 +4,40 @@
 
 // You can return the answer in any order.
 
-// nested for loops that traverse thru the array finding the sum that is equal to the target.
+// put array values as hashmap key and then use hashmap to find the target - value
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int first = 0;
-        int second = 0;
-        for(int i = 0; i<nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    first = i;
-                    second = j;
-                    return new int[]{first, second};
+        // int first = 0;
+        // int second = 0;
+        // for(int i = 0; i<nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             first = i;
+        //             second = j;
+        //             return new int[]{first, second};
+        //         }
+        //     }
+        // }
+        // return new int[]{first, second};
+        
+        HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            h.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int remainder = target - nums[i];
+            
+            if (h.containsKey(remainder)) {
+                int index = h.get(remainder);
+                if (index != i) {
+                    return new int[]{i, index};
                 }
             }
         }
-        return new int[]{first, second};
+        
+        return new int[]{};
     }
 }
